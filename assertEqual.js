bf9a90b1b181305ad63d14ff847f05a9ecec5a26
remained3 <sarahthebush@hotmail.com>
2021-09-13 20:52:11 +0000

    Code to make sure elements are equal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("⭐⭐⭐ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(1, -1);
