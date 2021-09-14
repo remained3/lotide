708ebb987a5ee9156481ed304ec27cdaa835ef3b
remained3 <sarahthebush@hotmail.com>
2021-09-14 13:28:31 +0000

    made a simple equal checker

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("⭐⭐⭐ Assertion Passed: " + actual + " === " + expected);
+    console.log(`⭐⭐⭐ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
