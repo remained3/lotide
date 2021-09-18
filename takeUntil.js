d585dd7e8c9836642af43b3a18e83fba61ce296c
remained3 <sarahthebush@hotmail.com>
2021-09-18 17:24:14 +0000

    loop through array until stated condition

---
+++ takeUntil.js
@@ -0,0 +1,37 @@
+const takeUntil = function(array, callback) {
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return output
+    } output.push(array[i]);
+  } return output;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else for (let i =0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`⭐⭐⭐ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  };
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
