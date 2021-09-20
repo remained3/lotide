b2463d688b04fadf01e852c3fd59aac5cfcef09e
remained3 <sarahthebush@hotmail.com>
2021-09-20 19:35:21 +0000

    removed tests to separate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false
-  } else for (let i =0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    }
-  } return true
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
@@ -16,9 +8,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-//Test Code
-
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2], [1,3]);
-assertArraysEqual(['hello'], ['hello']);
-assertArraysEqual(['hello'], ['hi']);
+module.exports = assertArraysEqual;
